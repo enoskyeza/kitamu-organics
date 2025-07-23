effe5341047f34e30941d8ee4c058d54714e2e62
enoskyeza <enoskyeza@gmail.com>
2025-07-23 18:53:44 +0300

    update the about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import ComingSoon from "@/components/pages/comingSoon";
+// import ComingSoon from "@/components/pages/comingSoon";
+
+import HeroSection from "@/components/sections/hero";
+import AboutSection from "@/components/sections/about";
+import ServicesSection from "@/components/sections/services";
+import VideoStorySection from "@/components/sections/project";
+import ProductsSection from "@/components/sections/products";
+import TeamSection from "@/components/sections/team";
+import Gallery from "@/components/sections/gallery";
 
 export default function Home() {
   return (
     <>
-      <ComingSoon />
+      <HeroSection />
+      <AboutSection />
+        <ServicesSection />
+        <VideoStorySection />
+        <ProductsSection />
+        <TeamSection />
+        <Gallery />
+      {/*<ComingSoon />*/}
     </>
   );
 }
